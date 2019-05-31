bed4f64bc1ea3566a9de78a3a0d45060199f1db3
Radu Pisoi <radupisoi23@gmail.com>
2019-05-31 19:16:57 +0300

    second change

--- src/main/java/ro/sync/TestReleasePlugin.java
+++ src/main/java/ro/sync/TestReleasePlugin.java
@@ -3,5 +3,6 @@ package ro.sync;
 public class TestReleasePlugin {
 	public static void main(String[] args) {
 		System.out.println("Line added in the first feature");
+		System.out.println("Line second in the first feature");
 	}
 }
