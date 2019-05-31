61a3e1df342b79170d7f2bb153c8070f42fee307
Radu Pisoi <radupisoi23@gmail.com>
2019-05-31 19:38:56 +0300

    other changes

--- src/main/java/ro/sync/TestReleasePlugin.java
+++ src/main/java/ro/sync/TestReleasePlugin.java
@@ -8,5 +8,7 @@ public class TestReleasePlugin {
 	public static void main(String[] args) {
 		System.out.println("Line added in the first feature");
 		System.out.println("Line second in the first feature");
+		
+		// other changes
 	}
 }
